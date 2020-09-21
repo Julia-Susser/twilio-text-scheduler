f1995d58975bb53e3276b93daeefcb5b8b565cb7
Julia-Susser <you@example.com>
2020-09-21 11:44:08 -0400

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 
 app.post('/sms', (req, res) => {
-  res.send("hey")
   const twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
